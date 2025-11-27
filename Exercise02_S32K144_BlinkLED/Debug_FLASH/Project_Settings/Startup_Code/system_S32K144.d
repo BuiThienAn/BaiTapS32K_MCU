4066fe51855dd4f54fd96f29824a1ba0b9ce47f4
BuiThienAn <buithienan2862003@gmail.com>
2025-11-27 20:20:19 +0700

    Nop bai exercise 02 - Blink LED

---
+++ Exercise02_S32K144_BlinkLED/Debug_FLASH/Project_Settings/Startup_Code/system_S32K144.d
@@ -0,0 +1,14 @@
+Project_Settings/Startup_Code/system_S32K144.o: \
+ ../Project_Settings/Startup_Code/system_S32K144.c \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/device_registers.h \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/s32_core_cm4.h \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/S32K144.h \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/S32K144_features.h \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/devassert.h \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/system_S32K144.h
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/device_registers.h:
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/s32_core_cm4.h:
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/S32K144.h:
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/S32K144_features.h:
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/devassert.h:
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/system_S32K144.h:
---
+++ Exercise02_S32K144_BlinkLED/Debug_FLASH/src/main.d
@@ -0,0 +1,3 @@
+src/main.o: ../src/main.c \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/S32K144.h
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_BlinkLED_Exercise2/include/S32K144.h:
