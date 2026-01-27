0d2e0bbc3b29f2fa6f638dc72c83aee5c066164e
BuiThienAn <buithienan2862003@gmail.com>
2026-01-27 10:40:36 +0700

    Update folders

---
+++ S32K144_Bootloader_Mock1/Debug_FLASH/src/FLASH.d
@@ -0,0 +1,5 @@
+src/FLASH.o: ../src/FLASH.c \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_Bootloader_Mock1/include/S32K144.h \
+ ../src/FLASH.h
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_Bootloader_Mock1/include/S32K144.h:
+../src/FLASH.h:
---
+++ S32K144_Bootloader_Mock1/Debug_FLASH/src/HAL_GPIO.d
@@ -0,0 +1,4 @@
+src/HAL_GPIO.o: ../src/HAL_GPIO.c ../src/HAL_GPIO.h \
+ C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_Bootloader_Mock1/include/S32K144.h
+../src/HAL_GPIO.h:
+C:/Users/Admin/workspaceS32DS.3.6.4/S32K144_Bootloader_Mock1/include/S32K144.h:
